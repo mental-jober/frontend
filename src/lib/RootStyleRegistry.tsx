893ee20497a4168b5b9054158e7dbb66de2830f2
pildrums <there7788@gmail.com>
2023-09-06 22:21:57 +0900

    Fix: styled-components 렌더링 오류 해결

---
+++ src/lib/RootStyleRegistry.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useServerInsertedHTML } from "next/navigation";
+import { useStyledComponentsRegistry } from "@/lib/styled-components";
+
+export default function RootStyleRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [StyledComponentsRegistry, styledComponentsFlushEffect] =
+    useStyledComponentsRegistry();
+
+  useServerInsertedHTML(() => <>{styledComponentsFlushEffect()}</>);
+
+  return <StyledComponentsRegistry>{children}</StyledComponentsRegistry>;
+}
