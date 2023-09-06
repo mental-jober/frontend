30a17971ee861b8a256f41f255aa7737e6f30374
pildrums <there7788@gmail.com>
2023-09-06 22:19:43 +0900

    Design: 테마 declare 파일, 테마, 전역 스타일링 파일 추가

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,9 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    text: string;
+    background: string;
+    border: string;
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  text: "#000",
+  background: "#eee",
+  border: "#ccc",
+};
