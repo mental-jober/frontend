b405f6ce5ef186f40eac4d6b338eec4694814c1b
pildrums <there7788@gmail.com>
2023-09-08 20:12:59 +0900

    Refcator: page.tsx 내용 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,9 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+
 import styled from "styled-components";
 
 const Home = () => {
-  return (
-    <Test>
-      <Trigger asChild>
-        <button>Trigger</button>
-      </Trigger>
-      <ModalContent className="sm:max-w-[300px]">
-        <Header>짜잔</Header>
-        <Description>히힛</Description>
-      </ModalContent>
-    </Test>
-  );
+  return null;
 };
-
-const Test = styled(Dialog)`
-  width: 100%;
-  height: 100vh;
-  background: coral;
-`;
-
-const Trigger = styled(DialogTrigger)`
-  /* padding: 20px; */
-  background: coral;
-  &:hover {
-    background: ${(props) => props.theme.background};
-  }
-`;
-
-const ModalContent = styled(DialogContent)``;
-
-const Header = styled(DialogHeader)``;
-
-const Description = styled(DialogDescription)``;
 
 export default Home;
