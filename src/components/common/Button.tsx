186290ef08e175f3882ac6d9034e54d6d2c95ac7
pildrums <there7788@gmail.com>
2023-09-11 20:12:25 +0900

    Design: normal 속성 커스텀 버튼 완성

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,15 +1,31 @@
-import { styled } from "styled-components";
+import { css, styled } from "styled-components";
 
 interface ButtonProps {
-  [rest: string]: any;
+  [props: string]: any;
 }
 
-const Button = ({ rest }: ButtonProps) => {
-  return <StyledButton {...rest} />;
+const Button = ({ ...props }: ButtonProps) => {
+  return <StyledButton {...props} />;
 };
 
-const StyledButton = styled.button`
-  padding: 20px;
+const StyledButton = styled.button<{ $normal?: boolean }>`
+  padding: 10px 20px;
+  background: ${(props) => props.theme.gray.normal};
+  border-radius: 4px;
+  font-weight: 800;
+  border: none;
+  outline: none;
+  &:hover {
+    background: ${(props) => props.theme.gray.normalHover};
+  }
+  /* props 이름 앞에 $ 명시해야 경고가 뜨지 않습니다! */
+  /* $: 임의의 props를 DOM까지 전달하게 만들어줍니다.*/
+  /* 실 사용은 app/page.tsx를 참고해주세요! */
+  ${(props) =>
+    props.$normal &&
+    css`
+      background: ${(props) => props.theme.blue[3]};
+    `}
 `;
 
 export default Button;
