1565c96e7860750cc986a15dfb1e9269c898b5a3
pildrums <there7788@gmail.com>
2023-09-11 19:06:56 +0900

    Refactor: GlobalStyle 컴포넌트 제외

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@
 import "@/styles/globals.css";
 import { ReactNode } from "react";
 import RootStyleRegistry from "@/lib/RootStyleRegistry";
-import GlobalStyle from "@/styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@/styles/theme";
 import ReactQueryProviders from "@/queries/queryProvider";
@@ -13,7 +12,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="ko">
       <ThemeProvider theme={theme}>
         <body>
-          <GlobalStyle />
           <ReactQueryProviders>
             <RootStyleRegistry>{children}</RootStyleRegistry>
           </ReactQueryProviders>
