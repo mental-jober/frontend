7e7a85a058ec9f68ff4a3773d5318917c54aeddf
BearHumanS <bye0651@naver.com>
2023-09-14 05:40:23 +0900

    FEAT: profile page text input부분

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState, ChangeEvent } from "react";
+import style from "./TextArea.module.css";
 
 export const MyTextArea = () => {
   const [text, setText] = useState("");
 
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const inputText = e.target.value;
 
     if (inputText.length <= 100) {
@@ -19,7 +20,7 @@ export const MyTextArea = () => {
         value={text}
         onChange={handleChange}
         placeholder="소개글을 입력해주세요.&#13;&#10;(최대 100자)"
-        className="w-[360px] h-[172px] bg-[#F5F5F5] rounded-lg Mytextarea"
+        className={`${style.Mytextarea} w-full min-w-[357px] h-[172px] bg-[#F5F5F5] rounded-lg`}
       ></textarea>
       <div className="text-right mt-3 h-[23px]">({text.length}/100)</div>
     </div>
@@ -29,7 +30,7 @@ export const MyTextArea = () => {
 export const SpaceNametextarea = () => {
   const [text, setText] = useState("");
 
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const inputText = e.target.value;
 
     setText(inputText);
@@ -41,7 +42,7 @@ export const SpaceNametextarea = () => {
         value={text}
         onChange={handleChange}
         placeholder="스페이스 이름을 입력해주세요."
-        className="w-[360px] h-[45px] bg-[#F5F5F5] rounded-lg SpaceNametextarea"
+        className={`${style.SpaceNametextarea} w-full min-w-[357px] h-[45px] bg-[#F5F5F5] rounded-lg`}
       ></textarea>
     </div>
   );
