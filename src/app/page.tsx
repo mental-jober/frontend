8b9a150d16bc524ab6eb464d6337eec26dd38fa0
pildrums <there7788@gmail.com>
2023-09-15 16:35:35 +0900

    Refactor: 테스트 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 "use client";
-
-import NotFoundMemberModal from "@/components/modal/NotFoundMemberModal";
-import useModal from "../../hooks/UseModalHook";
+import Input from "@/components/common/Input";
 
 const Home = () => {
-  const { isOpen, onCloseModal, onOpenModal } = useModal();
-
   return (
     <>
-      <div>
-        <button onClick={onOpenModal}>모달 열기</button>
-      </div>
-      {isOpen && <NotFoundMemberModal onClose={onCloseModal} />}
+      <Input placeholder="하이" />
     </>
   );
 };
