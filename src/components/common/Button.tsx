ca093198a309e03a3b4b96af53d9ef3932ab089a
BearHumanS <bye0651@naver.com>
2023-09-16 20:46:23 +0900

    Design: 공통 사용 버튼 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,26 +4,52 @@ interface ButtonProps {
   [props: string]: any;
 }
 
+type customProps = {
+  $normal?: boolean;
+  $save?: boolean;
+};
+
 const Button = ({ ...props }: ButtonProps) => {
   return <StyledButton {...props} />;
 };
 
-const StyledButton = styled.button<{ $normal?: boolean }>`
+const StyledButton = styled.button<customProps>`
   padding: 10px 20px;
-  background: ${({theme}) => theme.gray.normal};
+  background: ${({ theme }) => theme.gray.normal};
   border-radius: 4px;
   font-weight: 800;
   border: none;
   outline: none;
+
   &:hover {
-    background: ${({theme}) => theme.gray.normalHover};
+    background: ${({ theme }) => theme.gray.normalHover};
   }
-  /* props 이름 앞에 $ 명시해야 경고가 뜨지 않습니다! */
-  /* $: 임의의 props를 DOM까지 전달하게 만들어줍니다.*/
-  /* 실 사용은 app/page.tsx를 참고해주세요! */
+
   ${(props) =>
     props.$normal &&
     css`
+      padding: 30px; // $normal일 때의 padding
+      background: ${(props) => props.theme.blue[3]};
+    `}
+
+  ${(props) =>
+    props.$save &&
+    css`
+      display: flex;
+      width: 320px;
+      height: 50px;
+      padding: 10px;
+      justify-content: center;
+      align-items: center;
+      gap: 10px;
+      flex-shrink: 0;
+      border-radius: 10px;
+      color: #fff;
+      font-family: Inter;
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
       background: ${(props) => props.theme.blue[3]};
     `}
 `;
