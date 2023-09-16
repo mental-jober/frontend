26a45e4501db4fb1a0df8ee6434c730f3bde826d
ChaDongmin <cdm1263@gmail.com>
2023-09-16 19:52:14 +0900

    Design: #9 기획 디자인으로 수정

--- src/components/spaceLayout/block/BlockLink.tsx
+++ src/components/spaceLayout/block/BlockLink.tsx
@@ -8,7 +8,15 @@ export default function BlockLink({ link }: BlockLinkProps) {
   return <LinkBox disabled placeholder="URL을 입력해주세요"></LinkBox>;
 }
 const LinkBox = styled.input`
-  background: #ededed;
   width: 218px;
   height: 22px;
+  font-size: 16px;
+  font-weight: 500;
+  color: #b3b3b3;
+  padding-left: 5px;
+  font-style: normal;
+  border-radius: 5px;
+  line-height: normal;
+  background: #ededed;
+  letter-spacing: -0.32px;
 `;
