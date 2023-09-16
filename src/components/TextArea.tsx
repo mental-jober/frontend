0a3a34b56ed78c413e67609b90d0d38303f2e027
BearHumanS <bye0651@naver.com>
2023-09-16 20:39:33 +0900

    Design: textarea 디자인 수정

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,35 +1,9 @@
 "use client";
 
-import React, { ChangeEvent } from "react";
-import style from "./TextArea.module.css";
-import useStore from "@/lib/store/store.module";
-
-export const MyTextArea = () => {
-  const { currentText, setCurrentText } = useStore();
-
-  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const inputText = e.target.value;
-
-    if (inputText.length <= 100) {
-      setCurrentText(inputText);
-    }
-  };
-
-  return (
-    <div>
-      <textarea
-        value={currentText}
-        onChange={handleChange}
-        placeholder="소개글을 입력해주세요.&#13;&#10;(최대 100자)"
-        className={`${style.Mytextarea} w-full min-w-[357px] h-[172px] bg-[#F5F5F5] rounded-lg`}
-      ></textarea>
-      <div className="text-right mt-3 h-[23px]">({currentText.length}/100)</div>
-    </div>
-  );
-};
+import React, { ChangeEvent, useState } from "react";
 
 export const SpaceNametextarea = () => {
-  const { titleText, setTitleText } = useStore();
+  const [titleText, setTitleText] = useState("");
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const inputText = e.target.value;
@@ -38,12 +12,35 @@ export const SpaceNametextarea = () => {
   };
 
   return (
-    <div className="mt-[23px]">
+    <div className="mt-4">
       <textarea
         value={titleText}
         onChange={handleChange}
         placeholder="스페이스 이름을 입력해주세요."
-        className={`${style.SpaceNametextarea} w-full min-w-[357px] h-[45px] bg-[#F5F5F5] rounded-lg`}
+        className="w-full h-[45px] shrink-0 bg-[#ECF1F7] rounded-lg border-0 p-0"
+      ></textarea>
+    </div>
+  );
+};
+
+export const MyTextArea = () => {
+  const [titleText, setTitleText] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const inputText = e.target.value;
+
+    if (inputText.length <= 100) {
+      setTitleText(inputText);
+    }
+  };
+
+  return (
+    <div className="mt-4">
+      <textarea
+        value={titleText}
+        onChange={handleChange}
+        placeholder="소개글을 입력해주세요.(최대 100자)"
+        className="w-full h-[172px] shrink-0 bg-[#ECF1F7;] rounded-lg border-0 p-0"
       ></textarea>
     </div>
   );
