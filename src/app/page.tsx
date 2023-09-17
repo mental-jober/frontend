d584c963a1216d7e15dd221b6f49d87eb453ec53
pildrums <there7788@gmail.com>
2023-09-17 19:28:37 +0900

    Design: Header 컴포넌트를 위한 상단 마진 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
 "use client";
 
+import Header from "@/components/common/Header";
+import IntroProfile from "@/components/spaceLayout/IntroProfile";
+import IntroProjectText from "@/components/spaceLayout/intro/IntroProjectText";
+import { styled } from "styled-components";
+
 const Home = () => {
-  return <></>;
+  return (
+    <MainBlock>
+      <Header />
+      <HeaderMargin />
+      <IntroProfile />
+      <IntroProjectText />
+    </MainBlock>
+  );
 };
+
+const MainBlock = styled.div`
+  height: 100vh;
+`;
+
+const HeaderMargin = styled.div`
+  margin-top: 58px;
+`;
 
 export default Home;
