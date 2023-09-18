b731ec5418fcd45e52a2bffb4db01acbbc8d6a14
BearHumanS <bye0651@naver.com>
2023-09-18 22:31:50 +0900

    Test: ToastUi 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 "use client";
 
+import ToastUi from "@/components/toast/ToastUi";
+import { useToastStore } from "@/lib/store/store.module";
+
 const Home = () => {
-  return <></>;
+  const { showToast } = useToastStore();
+
+  return (
+    <>
+      <div>
+        <button onClick={() => showToast("저장 중~~")}>클릭</button>
+      </div>
+      <ToastUi />
+    </>
+  );
 };
 
 export default Home;
