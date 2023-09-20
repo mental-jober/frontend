bf23c94c6367d8adc4f7ef1492a96653cd1c8653
pildrums <there7788@gmail.com>
2023-09-21 05:18:53 +0900

    Feat: 카테고리 템플릿 페이지에 렌더링
    1. category 초기값 및 세터함수 정의 완료
    2. onSelect 함수 제작 완료
    3. Categories 컴포넌트에 props 전달 완료

--- src/app/template/page.tsx
+++ src/app/template/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import Header from "@/components/common/Header";
+import Categories from "@/components/template/Categories";
 import TemplateItem from "@/components/template/TemplateItem";
 import TemplateList from "@/components/template/TemplateList";
+import { useCallback, useState } from "react";
 import { styled } from "styled-components";
 const TemplatePage = () => {
+  const [category, setCategory] = useState("all");
+  const onSelect = useCallback((category: string) => setCategory(category), []);
   return (
     <Test>
       <Header />
+      <Categories category={category} onSelect={onSelect} />
       <TemplateList>
         <TemplateItem />
         <TemplateItem />
@@ -20,6 +25,6 @@ const TemplatePage = () => {
 
 const Test = styled.div`
   height: 100vh;
-`
+`;
 
 export default TemplatePage;
