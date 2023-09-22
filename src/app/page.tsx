b0a7eae849f023d65b70d60e5ab5c4c1cd74915d
iskra17 <rayanami@naver.com>
2023-09-22 15:34:52 +0900

    Rename : 홈화면 레이아웃을 '/' 로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import ToastUi from "@/components/toast/ToastUi";
-import { useToastStore } from "@/lib/store/store.module";
+import HomeLayout from '@/components/home/HomeLayout';
+// import ToastUi from "@/components/toast/ToastUi";
+// import { useToastStore } from "@/lib/store/store.module";
 
 const Home = () => {
-  const { showToast } = useToastStore();
+  // const { showToast } = useToastStore();
 
   return (
     <>
-      <div>
+    <HomeLayout />
+      {/* <div>
         <button onClick={() => showToast("저장 중~~")}>클릭</button>
       </div>
-      <ToastUi />
+      <ToastUi /> */}
     </>
   );
 };
