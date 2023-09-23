d44297f5db0bda30d98ecbd0d126345e1ab53b2f
ChaDongmin <cdm1263@gmail.com>
2023-09-24 01:06:24 +0900

    Feat: #26 저장됨 텍스트 생성

---
+++ src/components/spaceLayout/history/HistorySaved.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const HistorySaved = () => {
+  return <Text>저장됨</Text>;
+};
+
+const Text = styled.span`
+  font-size: 12px;
+  font-weight: 600;
+  color: #9aa8b8;
+  text-align: center;
+  line-height: normal;
+  letter-spacing: -0.24px;
+`;
+
+export default HistorySaved;
