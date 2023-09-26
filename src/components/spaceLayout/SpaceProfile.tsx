05525a4209f077c128701a467b3f14c0169c2d6f
ChaDongmin <cdm1263@gmail.com>
2023-09-26 13:58:05 +0900

    Feat: 임시 프로필형 스페이스 구현

--- src/components/spaceLayout/SpaceProfile.tsx
+++ src/components/spaceLayout/SpaceProfile.tsx
@@ -1,7 +1,24 @@
+"use client";
+
 import styled from "styled-components";
 import Plates from "./Plates";
-import Block from "./Block";
+// import Block from "./Block"; // 중간 배포 도중에 사용되지않아서 eslint오류
+import DraggableBlocks from "./DraggableBlocks";
 import IntroProfile from "./IntroProfile";
+
+export interface BlockData {
+  id: string;
+  name: string;
+  hidden: boolean;
+}
+
+// 임시 데이터
+// 추후 데이터를 zustand store에 추가해 데이터를 변경할 수 있도록 구성
+const initialBlocks: BlockData[] = [
+  { id: "task-1", name: "cont", hidden: false },
+  { id: "task-2", name: "page", hidden: false },
+  { id: "task-3", name: "temp", hidden: false },
+];
 
 // page로 이동 예정
 export default function SpaceProfile() {
@@ -9,13 +26,15 @@ export default function SpaceProfile() {
     <Container>
       <IntroProfile />
       <Plates />
+      <DraggableBlocks blockData={initialBlocks} />
     </Container>
   );
 }
 
 const Container = styled.div`
-  gap: 10px;
-  width: 360px;
+  gap: 18px;
+  min-width: 360px;
+  max-width: 430px;
   display: flex;
   align-items: center;
   flex-direction: column;
