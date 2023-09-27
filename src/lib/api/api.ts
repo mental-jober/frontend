87ab5421386bd255dcf86a698b81e3b5ad75b61b
iskra17 <rayanami@naver.com>
2023-09-27 17:28:54 +0900

    Add : accessToken 불러오기. 로그인, 회원가입 api 추가

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,18 +1,62 @@
+import { useUserStore } from '../store/useUserStore';
 import { client } from "./client";
 
 type Method = "get" | "post" | "put" | "delete";
 
+const accessToken = useUserStore.getState().user.accessToken;
+
 // 공용 fetch함수
-const fetchData = async (url: string, method: Method, reqData?: unknown) => {
+export const fetchData = async (
+  url: string,
+  method: Method,
+  reqData?: unknown,
+) => {
   try {
-    const { data } = await client({ url, method, data: reqData });
-    return data;
+    const response = await client({ url, method, data: reqData });
+    return response
   } catch (e) {
     console.error(e);
   }
 };
 
-// fetchData 사용예시
-export const exampleApi = () => {
-  return fetchData("/template", "get");
+client.interceptors.request.use(
+  function (config) {
+    if (!accessToken) {
+      return config;
+    }
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
+client.interceptors.response.use(
+  function (config) {
+    if (config.status === 404) {
+      console.log("404 Not Found");
+    }
+    return config;
+  },
+  function (error) {
+    Promise.reject(error);
+  },
+);
+
+export const loginApi = async (email: string, password: string) => {
+  const requestBody = {
+    email,
+    password,
+  };
+  return fetchData("/login", "post", requestBody);
+};
+
+export const signupApi = async (email: string, password: string, username: string) => {
+  const requestBody = {
+    email,
+    password,
+    username,
+  };
+  return fetchData("https://jober-workspace.azurewebsites.net/join", "post", requestBody);
 };
