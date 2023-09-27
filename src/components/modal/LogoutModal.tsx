a9897bb03266f5598a25c99558b4283731f1b3ab
iskra17 <rayanami@naver.com>
2023-09-28 00:52:59 +0900

    Feat : 로그아웃 모달 쿠키 삭제 구현

--- src/components/modal/LogoutModal.tsx
+++ src/components/modal/LogoutModal.tsx
@@ -7,22 +7,20 @@ import {
 import TitleHeader from "./AlertTitleHeader";
 import { ModalProps } from "../../../hooks/UseModalHook";
 import Button from "../common/Button";
-import { useUserStore } from "@/lib/store/useUserStore";
 import { useRouter } from "next/navigation";
 import { logoutApi } from "@/lib/api/api";
+import { deleteAccessTokenCookie } from "@/lib/cookies";
 
 const LogoutModal = ({ isOpen, onCloseModal }: ModalProps) => {
   const router = useRouter();
-  const { clearUser } = useUserStore();
 
   const handleLogout = async () => {
     try {
-      const response = await logoutApi(); // 로그아웃 API 호출
-      console.log("로그아웃response : ", response);
-      if (response?.status === 200) {
-        clearUser(); // Zustand store의 유저 정보 초기화
-        router.push("/auth/login"); // 로그인 페이지로 이동
-      }
+      // const response = await logoutApi(); // 로그아웃 API 호출
+      // if (response?.status === 200) {
+      // }
+      deleteAccessTokenCookie();
+      router.push("/auth/login"); // 로그인 페이지로 이동
     } catch (e) {
       console.error(e);
     }
