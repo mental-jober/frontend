ae48fb2437a758b8acf1c6e72ee4fc03e4304c11
iskra17 <rayanami@naver.com>
2023-09-27 23:39:11 +0900

    Feat : 로그아웃 기능 구현중(백엔드 리다이렉트 기능 대기)

--- src/components/modal/LogoutModal.tsx
+++ src/components/modal/LogoutModal.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   AlertCommonModal,
@@ -6,17 +7,46 @@ import {
 import TitleHeader from "./AlertTitleHeader";
 import { ModalProps } from "../../../hooks/UseModalHook";
 import Button from "../common/Button";
+import { useUserStore } from "@/lib/store/useUserStore";
+import { useRouter } from "next/navigation";
+import { logoutApi } from "@/lib/api/api";
 
 const LogoutModal = ({ isOpen, onCloseModal }: ModalProps) => {
+  const router = useRouter();
+  const { clearUser } = useUserStore();
+
+  const handleLogout = async () => {
+    try {
+      const response = await logoutApi(); // 로그아웃 API 호출
+      console.log("로그아웃response : ", response);
+      if (response?.status === 200) {
+        clearUser(); // Zustand store의 유저 정보 초기화
+        router.push("/auth/login"); // 로그인 페이지로 이동
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <>
       <AlertCommonModal isOpen={isOpen} onCloseModal={onCloseModal}>
         <TitleHeader title="정말 로그아웃 하시겠습니까?" />
         <ButtonContainer>
-          <Button $leftbtn="true" onClick={() => {}}>
+          <Button
+            $leftbtn="true"
+            onClick={() => {
+              onCloseModal();
+            }}
+          >
             취소
           </Button>
-          <Button $rightbtn="true" onClick={() => {}}>
+          <Button
+            $rightbtn="true"
+            onClick={() => {
+              handleLogout();
+            }}
+          >
             확인
           </Button>
         </ButtonContainer>
