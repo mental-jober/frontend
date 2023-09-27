f3be5ba6b27d5346f7e548b610bfc1bc283d71e6
iskra17 <rayanami@naver.com>
2023-09-28 00:48:48 +0900

    Rename : 토큰 유효성 검사 위치 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,13 @@ import ReactQueryProviders from "@/queries/queryProvider";
 import GlobalStyle from "@/styles/GlobalStyle";
 import StyledComponentsRegistry from "@/lib/registry";
 import Margin from "@/components/common/Margin";
+import useAuthCheck from "@/components/auth/useAuthCheck";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  // TODO: 토큰 유효성 검사 api 호출
+  useAuthCheck();
+
   return (
-    // TODO: 토큰 유효성 검사 api 호출
     <html lang="ko">
       <ThemeProvider theme={theme}>
         <body>
