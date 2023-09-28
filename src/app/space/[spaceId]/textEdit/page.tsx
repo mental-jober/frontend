c1910eb60903c0ea4f21d5f12de5fb802daeb570
BearHumanS <bye0651@naver.com>
2023-09-28 18:02:54 +0900

    Refactor: 일부 코드 수정 및 api 헤더에 accessToken

--- src/app/space/[spaceId]/textEdit/page.tsx
+++ src/app/space/[spaceId]/textEdit/page.tsx
@@ -5,12 +5,10 @@ import Header from "@/components/common/Header";
 import TextEditor from "@/components/textEditor/FroalaTextEditor";
 import ToastUi from "@/components/toast/ToastUi";
 import { froalaEditorStore, useToastStore } from "@/lib/store/store.module";
-import { ChangeEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const TextEditPage = () => {
-  const [titleText, setTitleText] = useState("");
   const { text } = froalaEditorStore();
-  const [prevTitleText, setPrevTitleText] = useState("");
   const [prevText, setPrevText] = useState("");
   const [isBtnDisabled, setIsBtnDisabled] = useState(true);
 
@@ -25,26 +23,14 @@ const TextEditPage = () => {
   }, [showToast]);
 
   useEffect(() => {
-    setIsBtnDisabled(
-      !text || !titleText || (text === prevText && titleText === prevTitleText),
-    );
-  }, [text, titleText, prevText, prevTitleText]);
-
-  const onTitleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setTitleText(e.target.value);
-  };
+    setIsBtnDisabled(!text || text === prevText);
+  }, [text, prevText]);
 
   const onSave = () => {
-    console.log("onSave 함수 호출됨");
-    console.log("titleText:", titleText);
-    console.log("text:", text);
-
-    if (!titleText || !text) {
-      console.log("제목 또는 내용이 비어 있음");
-      alert("제목 또는 내용이 비었습니다.");
+    if (!text) {
+      alert("내용이 비었습니다.");
       return;
     }
-    setPrevTitleText(titleText);
     setPrevText(text);
   };
 
@@ -52,14 +38,6 @@ const TextEditPage = () => {
     <>
       <div className="flexable flex-col">
         <Header />
-        <div className="mt-6 w-full">
-          <textarea
-            placeholder="이름을 입력해주세요."
-            className="w-full h-[27px] border-0 p-0 text-[20px]"
-            value={titleText}
-            onChange={onTitleChange}
-          />
-        </div>
         <div className="mt-4">
           <TextEditor />
         </div>
