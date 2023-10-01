1fddfec0e5eae846b9f6f88e726e8d691c35a69c
pildrums <there7788@gmail.com>
2023-10-01 16:26:38 +0900

    Fix: 쿠키로 저장된 accessToken으로 변경

--- src/lib/api/templateAPI.ts
+++ src/lib/api/templateAPI.ts
@@ -1,8 +1,8 @@
+import { getAccessTokenCookie } from "../cookies";
 import { fetchData } from "./api";
 import { client } from "./client";
 
-const accessToken =
-  "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MjEsImVtYWlsIjoicGlsamluQGdtYWlsLmNvbSIsImFzc2lnbmVkU3BhY2VNZW1iZXJJbmZvcyI6Int9IiwiaWF0IjoxNjk1NzkwNzE0LCJleHAiOjE2OTU4NzcxMTR9.W5o8fcnR7vNuzrvXWWxjfOsqdB7IlPCLIzM69QblLSA";
+const accessToken = getAccessTokenCookie();
 
 client.interceptors.request.use(
   function (config) {
@@ -28,13 +28,6 @@ client.interceptors.response.use(
     Promise.reject(error);
   },
 );
-
-export function login() {
-  return fetchData("/login", "post", {
-    email: "piljin@gmail.com",
-    password: "apaqj11!",
-  });
-}
 
 function encode(keyword: string) {
   const rLang = /[ㄱ-ㅎ | ㅏ-ㅣ | 가-힣]/;
