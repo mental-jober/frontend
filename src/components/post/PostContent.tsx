d8b3eed920ac28cd81578c89698f2a4c45991ed9
kkaeun <kkydu007@naver.com>
2023-10-02 20:57:11 +0900

    Feat:#28 PostContent 컴포넌트 완성

---
+++ src/components/post/PostContent.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+// const ContentData = [
+//   {
+//     id: 1,
+
+//   },
+
+//   { id: 2 },
+// ];
+
+const PostContent = () => {
+  return (
+    <PostContentBox>
+      <a>test content</a>
+      {/* {ContentData.map} */}
+    </PostContentBox>
+  );
+};
+
+const PostContentBox = styled.div``;
+
+export default PostContent;
