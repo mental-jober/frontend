bead1c20d04b771e29571b38670d665ae5725196
ChaDongmin <cdm1263@gmail.com>
2023-10-02 20:54:46 +0900

    Refactor: #43 SpaceData 수정 및 데이터 기본값 추가

--- src/lib/store/useSpaceStore.ts
+++ src/lib/store/useSpaceStore.ts
@@ -1,20 +1,23 @@
 import { create } from "zustand";
 
 export interface SpaceData {
-  createMember: number;
   url: string;
   title: string;
   description: string;
   profileImageUrl: string;
   backgroundImageUrl: string;
+  componentList: [];
+  createMemberId: number;
   pathIds: string;
   authorized: boolean;
   sequence: number;
+  createdAt: string;
+  updatedAt: string;
 }
 
-interface useSpaceStore {
+interface SpaceStore {
   datas: Record<string, SpaceData>;
-  addData: (id: number, DataData: SpaceData) => void;
+  addData: (id: number, DataData?: SpaceData) => void;
   deleteData: (id: number) => void;
   getData: (id: number) => SpaceData | null;
   getValue: <K extends keyof SpaceData>(
@@ -28,10 +31,27 @@ interface useSpaceStore {
   ) => void;
 }
 
-const useSpaceStore = create<useSpaceStore>((set, get) => ({
+const useSpaceStore = create<SpaceStore>((set, get) => ({
   datas: {},
 
   addData: (id, newData) => {
+    if (!newData) {
+      newData = {
+        url: "",
+        title: "",
+        description: "",
+        profileImageUrl: "",
+        backgroundImageUrl: "",
+        componentList: [],
+        createMemberId: 0,
+        pathIds: "",
+        authorized: false,
+        sequence: 0,
+        createdAt: "",
+        updatedAt: "",
+      };
+    }
+
     set((state) => ({
       datas: {
         ...state.datas,
