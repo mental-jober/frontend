73de4df3d646cede70a60f181a0d29c09fd51319
BearHumanS <bye0651@naver.com>
2023-10-04 01:15:25 +0900

    FEAT: 리액트 쿼리 사용 및 드롭다운 type관련 상수로 변경

--- src/queries/queries.ts
+++ src/queries/queries.ts
@@ -1,11 +1,25 @@
-import { exampleApi } from "@/lib/api/api";
+/* import { exampleApi } from "@/lib/api/api"; */
 import { useQuery } from "react-query";
 import { queryKey } from "./queryKeys";
+import { checkMembers } from "@/lib/api/memberAPI";
 
 interface QueryOptions {
   [key: string]: unknown;
 }
 
-export const useExampleQuery = (params: string, options?: QueryOptions) => {
+/* export const useExampleQuery = (params: string, options?: QueryOptions) => {
   return useQuery([queryKey.EXAMPLE, params], exampleApi, { ...options });
+}; */
+
+export const useMemberCheckQuery = (
+  space_wall_id: number,
+  options?: QueryOptions,
+) => {
+  return useQuery(
+    [queryKey.MEMBER, space_wall_id],
+    () => checkMembers(space_wall_id),
+    {
+      ...options,
+    },
+  );
 };
