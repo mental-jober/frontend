2c2ba48dee461b7161f5d88df12cd2d5f557d6f6
BearHumanS <bye0651@naver.com>
2023-10-03 20:01:48 +0900

    FEAT: 토큰 체크를 통해서 토큰 만료 시에 쿠키에서 토큰 제거

--- src/components/auth/useAuthCheck.ts
+++ src/components/auth/useAuthCheck.ts
@@ -1,17 +1,34 @@
 "use client";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { getAccessTokenCookie } from '@/lib/cookies';
+import { deleteAccessTokenCookie, getAccessTokenCookie } from "@/lib/cookies";
+import { tokenCheck } from "@/lib/api/tokenCheckAPI";
 
 const useAuthCheck = (redirectUrl: string = "/auth/login") => {
   const router = useRouter();
   const accessToken = getAccessTokenCookie(); // Zustand store에서 accessToken 가져오기
 
+  const crossCheckToken = useCallback(async () => {
+    try {
+      const response = await tokenCheck();
+
+      if (response !== true || !response) {
+        deleteAccessTokenCookie();
+        router.refresh();
+      }
+    } catch (error) {
+      console.error("토큰 확인 에러:", error);
+    }
+  }, [router]);
+
   useEffect(() => {
+    crossCheckToken();
+
     if (!accessToken) {
       router.replace(redirectUrl); // accessToken이 없으면 redirectUrl로 리다이렉트
+      return;
     }
-  }, [accessToken, router, redirectUrl]);
+  }, [accessToken, router, redirectUrl, crossCheckToken]);
 };
 
 export default useAuthCheck;
