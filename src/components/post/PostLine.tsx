89d05d13ed6087adc276ea908ae66723fe4f49a8
kkaeun <kkydu007@naver.com>
2023-10-03 17:35:27 +0900

    Rename:#28PostLink로 컴포넌트 변경

--- src/components/post/PostLine.tsx
+++ src/components/post/PostLine.tsx
@@ -4,8 +4,18 @@ const PostMiddleLine = () => {
   return <Line />;
 };
 
+// 지우지 말 것!
+const LineBlock = styled.div`
+  width: 100%;
+  max-width: 430px;
+  min-width: 360px;
+  padding: 0 20px;
+  display: flex;
+  justify-content: center;
+`;
+
 const Line = styled.div`
-  width: 320px;
+  width: 100%;
   height: 2px;
   border-radius: 2px;
   background: var(--foundation-grey-900, #444a51);
