9ce39de42c28493c6a37eb1dfd8e63d5428ec77b
iskra17 <rayanami@naver.com>
2023-10-04 10:44:52 +0900

    Feat : 루트 페이지에 AuthCheck와 로딩 스피너 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+"use client";
+
+import useAuthCheck from "@/components/auth/useAuthCheck";
+import LoadingSpinner from "@/components/common/LoadingSpinner";
 import HomeLayout from "@/components/home/HomeLayout";
 
 const Home = () => {
-  
-  return (
-    <>
-      <HomeLayout />
-    </>
-  );
+  const isAuthenticated = useAuthCheck();
+
+  if (isAuthenticated === null) {
+    // 인증 상태 확인 중
+    return <LoadingSpinner />;
+  }
+
+  return <>{isAuthenticated ? <HomeLayout /> : null}</>;
 };
 
 export default Home;
