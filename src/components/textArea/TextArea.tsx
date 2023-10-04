b6c4f53fc74dfc3723d9e8df8bb825eda0137c25
BearHumanS <bye0651@naver.com>
2023-10-04 15:51:02 +0900

    Refactor: 상태관리 부분 전역 상태관리 수정

--- src/components/textArea/TextArea.tsx
+++ src/components/textArea/TextArea.tsx
@@ -1,22 +1,23 @@
 "use client";
 
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent } from "react";
 import style from "./TextArea.module.css";
+import { profileStore } from "@/lib/store/store.module";
 
 export const SpaceNameInput = () => {
-  const [titleText, setTitleText] = useState("");
+  const { profileTitle, setProfileTitle } = profileStore();
 
-  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+  const onTitleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const inputText = e.target.value;
 
-    setTitleText(inputText);
+    setProfileTitle(inputText);
   };
 
   return (
     <div className="mt-4">
       <textarea
-        value={titleText}
-        onChange={handleChange}
+        value={profileTitle}
+        onChange={onTitleChange}
         placeholder="스페이스 이름을 입력해주세요."
         className={`${style.SpaceNametextarea} w-full h-[45px] bg-[#ECF1F7] rounded-lg border-0 p-0`}
       />
@@ -25,21 +26,21 @@ export const SpaceNameInput = () => {
 };
 
 export const MyTextInput = () => {
-  const [titleText, setTitleText] = useState("");
+  const { profileText, setProfileText } = profileStore();
 
-  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+  const onTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const inputText = e.target.value;
 
     if (inputText.length <= 100) {
-      setTitleText(inputText);
+      setProfileText(inputText);
     }
   };
 
   return (
     <div className="mt-4">
       <textarea
-        value={titleText}
-        onChange={handleChange}
+        value={profileText}
+        onChange={onTextChange}
         placeholder="소개글을 입력해주세요.(최대 100자)"
         className={`${style.Mytextarea} w-full h-[172px] bg-[#ECF1F7;] rounded-lg border-0 p-0`}
       />
