a396a5f0e2c7070f30d4ba4f8a1af558dba14b80
ChaDongmin <cdm1263@gmail.com>
2023-10-05 02:26:11 +0900

    Feat: #43 레이아웃 선택 페이지에서 선택한 레이아웃 출력

--- src/app/space/[id]/edit/page.tsx
+++ src/app/space/[id]/edit/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import DraggableBlocks from "@/components/spaceLayout/DraggableBlocks";
+import IntroProfile from "@/components/spaceLayout/IntroProfile";
 import IntroProject from "@/components/spaceLayout/IntroProject";
 import Plates from "@/components/spaceLayout/Plates";
+import { createBlock } from "@/lib/api/spaceEditAPI";
 import useComponentStore from "@/lib/store/useComponentStore";
+import { usePageLayoutStore } from "@/lib/store/usePageLayoutStore";
 import useSpaceWallStore from "@/lib/store/useSpaceWallStore";
 
 import { useParams } from "next/navigation";
@@ -12,8 +15,9 @@ import styled from "styled-components";
 
 const EditPage = () => {
   const { spaceWallId, setSpaceWallId } = useSpaceWallStore();
-  const { getSpaceComponents } = useComponentStore();
+  const { getSpaceComponents, setSpaceComponents } = useComponentStore();
   const paramSpaceWallId = useParams().id;
+  const { type, composition } = usePageLayoutStore();
 
   useEffect(() => {
     if (typeof paramSpaceWallId === "string") {
@@ -22,14 +26,33 @@ const EditPage = () => {
       }
     }
   }, [spaceWallId, setSpaceWallId, paramSpaceWallId]);
+
+  useEffect(() => {
+    const onCreateLayoutComponents = async () => {
+      for (const type of composition) {
+        const blockData = {
+          parentSpaceWallTempId: 3, /// 원래는 spaceWallId를 넣어야 함
+          type: type,
+          sequence: Object.values(getSpaceComponents(spaceWallId as number))
+            .length,
+        };
+        const { data } = await createBlock(blockData);
+        setSpaceComponents(spaceWallId as number, data.componentTempId, data);
+      }
+    };
+
+    onCreateLayoutComponents();
+  }, [composition, getSpaceComponents, setSpaceComponents, spaceWallId]);
+
   return (
     <Container>
-      <IntroProject />
+      {type === "프로젝트형" ? <IntroProject /> : <IntroProfile />}
+
       <Plates />
       <DraggableBlocks
-        blockData={Object.values(
-          getSpaceComponents(spaceWallId as number),
-        ).sort((a, b) => a.sequence - b.sequence)}
+        blockData={Object.values(getSpaceComponents(spaceWallId as number))
+          .filter((component) => !component.deleted)
+          .sort((a, b) => a.sequence - b.sequence)}
       />
     </Container>
   );
