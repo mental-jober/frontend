e827c63326d3f6f471cc570fc4bf6be976d0002d
iskra17 <rayanami@naver.com>
2023-10-04 10:46:24 +0900

    Feat : useAuthCheck에 체크 토큰 api 적용

--- src/components/auth/useAuthCheck.ts
+++ src/components/auth/useAuthCheck.ts
@@ -1,17 +1,42 @@
-"use client";
-import { useEffect } from "react";
+// "use client";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { getAccessTokenCookie } from '@/lib/cookies';
+import { getAccessTokenCookie } from "@/lib/cookies";
+import { checkTokenApi } from "@/lib/api/api";
 
 const useAuthCheck = (redirectUrl: string = "/auth/login") => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
   const router = useRouter();
-  const accessToken = getAccessTokenCookie(); // Zustand store에서 accessToken 가져오기
+  const accessToken = getAccessTokenCookie();
 
   useEffect(() => {
-    if (!accessToken) {
-      router.replace(redirectUrl); // accessToken이 없으면 redirectUrl로 리다이렉트
-    }
+    const checkAuthentication = async () => {
+      if (!accessToken) {
+        setIsAuthenticated(false);
+        router.replace(redirectUrl);
+      } else {
+        try {
+          const res = await checkTokenApi();
+          console.log("CHECK-TOKEN ", res);
+          if (res === true || res.status === 200) {
+            console.log("res === true : 확인");
+            setIsAuthenticated(true);
+          } else {
+            setIsAuthenticated(false);
+            router.replace(redirectUrl);
+          }
+        } catch (e) {
+          console.error(e);
+          setIsAuthenticated(false);
+          router.replace(redirectUrl);
+        }
+      }
+    };
+
+    checkAuthentication();
   }, [accessToken, router, redirectUrl]);
+
+  return isAuthenticated;
 };
 
 export default useAuthCheck;
