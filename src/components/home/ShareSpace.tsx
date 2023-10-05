b8da22f924f96ba13b0bd848ce2c9b5685fd1e17
iskra17 <rayanami@naver.com>
2023-10-06 06:51:37 +0900

    Feat : 메인 공유스페이스 메뉴에서 임시저장 스페이스 불러오기 구현

--- src/components/home/ShareSpace.tsx
+++ src/components/home/ShareSpace.tsx
@@ -1,7 +1,7 @@
-import { getShareSpaceListApi } from "@/lib/api/api";
+import { getShareSpaceListApi, getTempSpaceByIdApi } from "@/lib/api/api";
 import { useUserStore } from "@/lib/store/useUserStore";
 import Image from "next/image";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 interface SpaceData {
@@ -13,6 +13,9 @@ interface SpaceData {
 const SharedSpace = () => {
   const router = useRouter();
   const [shareSpaceList, setShareSpaceList] = useState<SpaceData[]>([]);
+  const [tempSpaceFlags, setTempSpaceFlags] = useState<{
+    [key: number]: boolean;
+  }>({});
   const { user } = useUserStore();
 
   useEffect(() => {
@@ -30,7 +33,32 @@ const SharedSpace = () => {
       }
     };
     fetchShareSpaceList();
-  }, []);
+  }, []); // shareSpaceList를 가져오는 것만 담당
+
+  useEffect(() => {
+    const checkAllTempSpaces = async () => {
+      const newTempSpaceFlags: { [key: number]: boolean } = {};
+      for (const data of shareSpaceList) {
+        newTempSpaceFlags[data.spaceWallId] = await checkTempSpace(
+          data.spaceWallId,
+        );
+      }
+      setTempSpaceFlags(newTempSpaceFlags);
+    };
+    checkAllTempSpaces();
+  }, [shareSpaceList]); // shareSpaceList가 변경될 때 tempSpaceFlags를 업데이트
+
+  const checkTempSpace = async (spaceWallId: number) => {
+    try {
+      const tempSpace = await getTempSpaceByIdApi(spaceWallId);
+      if (tempSpace?.code === 200) {
+        return true;
+      }
+    } catch (e) {
+      console.error("Failed to check temp space:", e);
+    }
+    return false;
+  };
 
   const getAuthString = (auth: string) => {
     switch (auth) {
@@ -43,6 +71,10 @@ const SharedSpace = () => {
       default:
         return "알 수 없음";
     }
+  };
+
+  const handleLoadTempSpace = (id: number) => {
+    router.push(`/space/${id}/edit`);
   };
 
   return (
@@ -77,27 +109,39 @@ const SharedSpace = () => {
           />
         </div>
       </div>
-      <div className="text2-bold text-foundation-grey-700 mx-2 mb-2">기존 공유스페이스</div>
-      <div style={{ overflowY: 'auto', height: 'calc(100vh - 180px)' }}>
-      {shareSpaceList.map((data) => (
-        <div
-          key={data.spaceWallId}
-          className="flex flex-col justify-between gap-1 rounded-xl bg-foundation-grey-200 py-2 px-[14px] my-2"
-        >
-          <span className="text1-bold text-foundation-grey-700">
-            {data.title ?? "[제목없음]"}
-          </span>
-          <div className="flex justify-between gap-6 items-center">
-            <span className="text2-medium text-foundation-grey-700">
-              ID : {data.spaceWallId}
-            </span>
-            <span className="text2-medium text-foundation-grey-700">
-              권한 : {getAuthString(data.auths)}
-            </span>
+      <div className="text2-bold text-foundation-grey-700 mx-2 mb-2">
+        기존 공유스페이스
+      </div>
+      <div style={{ overflowY: "auto", height: "calc(100vh - 180px)" }}>
+        {shareSpaceList.map((data) => (
+          <div
+            key={data.spaceWallId}
+            className="flex flex-col justify-between gap-1 rounded-xl bg-foundation-grey-200 py-2 px-[14px] my-2"
+          >
+            <div className="flex justify-between gap-1 items-center">
+              <span className="text1-bold text-foundation-grey-700">
+                {data.title ?? "[제목없음]"}
+              </span>
+              {tempSpaceFlags[data.spaceWallId] && (
+                <button
+                  onClick={() => handleLoadTempSpace(data.spaceWallId)}
+                  className="text3-medium rounded-lg text-foundation-grey-700 bg-foundation-blue-200"
+                >
+                  불러오기
+                </button>
+              )}
+            </div>
+            <div className="flex justify-between gap-1 items-center">
+              <span className="text2-medium text-foundation-grey-700">
+                ID : {data.spaceWallId}
+              </span>
+              <span className="caption1-medium text-foundation-grey-700">
+                권한 : {getAuthString(data.auths)}
+              </span>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </div>
   );
 };
