577ffcd3dfb41c3f1bece1beeba3e3dd8079f302
ChaDongmin <cdm1263@gmail.com>
2023-10-06 05:46:29 +0900

    Feat: #43 임시저장, 임시데이터 조회, 편집모드 종료 API 추가

--- src/lib/api/spaceEditAPI.ts
+++ src/lib/api/spaceEditAPI.ts
@@ -1,13 +1,30 @@
+import { SpaceData } from "../store/useSpaceStore";
 import { fetchData } from "./api";
 
 interface NewSpaceData {
-  spaceWallId: number | null;
+  parentSpaceWallId: number | null;
 }
 
 interface NewBlockData {
   spaceWallId: number;
   type: string;
   sequence: number;
+}
+
+interface ComponentTempData {
+  componentTempId: number;
+  visible: boolean;
+  sequence: number;
+  deleted: boolean;
+}
+interface SaveSpaceData {
+  spaceWallTempId: number;
+  spaceWallId: number;
+  title: string;
+  description: string;
+  profileImageUrl: string;
+  backgroundImageUrl: string;
+  componentTempList: ComponentTempData[];
 }
 
 export const createSpace = async (spaceData: NewSpaceData) => {
@@ -27,4 +44,22 @@ export const createBlock = async (
 
 export const enterEdit = async (spaceWallId: string) => {
   return await fetchData(`spaceTemps/new/${spaceWallId}`, "post");
+};
+
+export const saveEditSpace = async (
+  spaceWallId: number,
+  spaceData: SpaceData,
+) => {
+  return await fetchData(`spaceTemps/save/${spaceWallId}`, "put", spaceData);
+};
+
+export const getEditSpace = async (spaceWallId: number) => {
+  return await fetchData(`spaceTemps/view/${spaceWallId}`, "get");
+};
+
+export const saveSpace = async (
+  spaceWallId: number,
+  spaceData: SaveSpaceData,
+) => {
+  return await fetchData(`spaceTemps/done/${spaceWallId}`, "put", spaceData);
 };
