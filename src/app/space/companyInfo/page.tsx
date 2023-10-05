723818ce80f0c86dd6fa8063ec9d371dd019dbca
KIMKAEUN <kkydu009@gmail.com>
2023-10-06 04:11:03 +0900

    Design:#28 companyInfo 전체 css 추가

--- src/app/space/companyInfo/page.tsx
+++ src/app/space/companyInfo/page.tsx
@@ -8,21 +8,28 @@ import PostTemplate from "@/components/post/PostTemplate";
 import IntroProfile from "@/components/spaceLayout/IntroProfile";
 import Link from "next/link";
 import styled from "styled-components";
+import React from "react";
+// import DropdownMenu from "@/components/post/DropDowun";
+// import { HiChevronRight } from "react-icons/hi"; // 화살표 아이콘
+// import { IoLinkOutline } from "react-icons/io5"; // 복사 아이콘
+
+// const items = [
+//   { id: 1, label: "공개범위", icon: <HiChevronRight /> },
+//   { id: 2, label: "공동 작업자 추가", icon: <HiChevronRight /> },
+//   { id: 3, label: "링크", icon: <IoLinkOutline /> },
+//   { id: 4, label: "전체보기로 보기" },
+// ];
 
 const dummyData = [
   {
     id: 1,
     title: "첫 번째 포스트",
     content: "첫 번째 포스트 내용",
-
-    // videoUrl:
-    //   "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
   },
   {
     id: 2,
     title: "두 번째 포스트",
     content: "두 번째 포스트 내용",
-
     videoUrl:
       "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
   },
@@ -31,15 +38,15 @@ const dummyData = [
     id: 3,
     title: "세 번째 포스트",
     content: "세 번째 포스트 내용",
-
-    // videoUrl:
-    //   "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
   },
 ];
 
 const CompanyInfoPage = () => {
   return (
     <>
+      {/* <div>
+        <DropdownMenu items={items} />
+      </div> */}
       <Header />
       <Margin />
       <CompanyInfoBlock>
@@ -53,24 +60,34 @@ const CompanyInfoPage = () => {
               <h2>{item.title}</h2>
               <PostLine />
               <p>{item.content}</p>
-              {item.videoUrl && ( // 동영상 URL이 존재할 때만 렌더링
+              {item.videoUrl && ( // 동영상 URL이 존재할 때만 렌더링//
                 <iframe
                   src={item.videoUrl}
                   title="#"
                   width="320px"
                   height="180px"
+                  style={{
+                    display: "flex",
+                    padding: "12px 0px",
+                  }}
                 ></iframe>
               )}
-              <PostLine />
             </PostContent>
           ))}
         </PostContentBlock>
-        <LinkPage />
+        <LinkList>
+          <StyledLink href="#">링크</StyledLink>
+          <StyledLink href="#">링크</StyledLink>
+          <StyledLink href="#">링크</StyledLink>
+        </LinkList>
         <PostTemplate />
         <LinkPage />
         <LinkPage />
+        <Line>
+          <PostLine />
+        </Line>
         <PostTemplate />
-        <PostTemplate />
+        <LinkPage />
       </CompanyInfoBlock>
     </>
   );
@@ -92,18 +109,6 @@ const SocialList = styled.div`
   align-items: center;
   margin-top: 18px;
   margin-bottom: 20px;
-  a {
-    &:nth-child(even)::before {
-      content: "|";
-      margin: 0 10px;
-      color: #000; // 나중에 색은 바꾸세요
-    }
-    &:nth-child(even)::after {
-      content: "|";
-      margin: 0 10px;
-      color: #000; // 나중에 색은 바꾸세요
-    }
-  }
 `;
 
 const PostContentBlock = styled.div`
@@ -111,7 +116,6 @@ const PostContentBlock = styled.div`
   max-width: 430px;
   min-width: 360px;
   padding: 0 20px;
-  /* gap:12px; */
 `;
 
 const PostContent = styled.div`
@@ -123,6 +127,7 @@ const PostContent = styled.div`
   }
   p {
     margin-top: 8px;
+    padding: 16px 4px;
   }
 `;
 
@@ -130,5 +135,24 @@ const StyledLink = styled(Link)`
   color: #2593fc;
   font-size: 14px;
   font-weight: 700;
+`;
+
+const LinkList = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 320px;
+  padding: 8px 4px;
+  gap: 10px;
+`;
+
+const Line = styled.div`
+  width: 320px;
+  max-width: 430px;
+  min-width: 360px;
+  padding: 0 20px;
+  height: 2px;
+  border-radius: 2px;
+  padding: 8px 0px;
+  background: var(#444a51);
 `;
 export default CompanyInfoPage;
