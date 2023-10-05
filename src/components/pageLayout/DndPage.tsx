7afb34a5c5b80b35cc96f8675af4c16c91ea06d2
iskra17 <rayanami@naver.com>
2023-10-05 09:56:19 +0900

    Fix : 햄버거바 DND 구현중

--- src/components/pageLayout/DndPage.tsx
+++ src/components/pageLayout/DndPage.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState } from "react";
 import {
   DragDropContext,
@@ -12,6 +13,7 @@ type Page = {
   title: string;
   templates: string[];
   isExpanded: boolean;
+  parentId?: string;
 };
 
 type Template = {
@@ -32,30 +34,35 @@ const initialData: InitialData = {
       title: "페이지 B",
       templates: ["template-2-1", "template-2-2"],
       isExpanded: false,
+      parentId: "page-1",
     },
     {
       id: "page-3",
       title: "페이지 C",
       templates: ["template-2-1", "template-2-2"],
       isExpanded: false,
+      parentId: "page-1",
     },
     {
       id: "page-4",
       title: "페이지 D",
       templates: ["template-2-1", "template-2-2"],
       isExpanded: false,
+      parentId: "page-1",
     },
     {
       id: "page-5",
       title: "페이지 E",
       templates: ["template-2-1", "template-2-2"],
       isExpanded: false,
+      parentId: "page-1",
     },
     {
       id: "page-6",
       title: "페이지 F",
       templates: ["template-2-1", "template-2-2"],
       isExpanded: false,
+      parentId: "page-1",
     },
     // ... (나머지 페이지와 템플릿)
   ],
@@ -71,28 +78,51 @@ function DndPage() {
 
   const onDragEnd = (result: DropResult) => {
     const { source, destination } = result;
-  
-    if (!destination) return; // 드롭 위치가 없는 경우
-  
+
+    if (!destination) return;
+
     const pages = Array.from(data.pages);
-    const [reorderedPage] = pages.splice(source.index, 1);
-  
-    // 상위 페이지와 하위 페이지 간 교환 로직
-    if (source.index === 0 && destination.index > 0) {
-      // 상위 페이지를 하위 페이지로 드래그하는 경우
-      const topPage = pages[0];
-      pages.splice(destination.index - 1, 0, reorderedPage); // 인덱스 조정
-      pages[0] = topPage;
-    } else if (destination.index === 0 && source.index > 0) {
-      // 하위 페이지를 상위 페이지로 드래그하는 경우
-      const prevTopPage = pages[0];
-      pages[0] = reorderedPage;
-      pages.splice(source.index, 0, prevTopPage);
-    } else {
-      // 일반적인 드래그&드롭
-      pages.splice(destination.index, 0, reorderedPage);
+    const [movedPage] = pages.splice(source.index, 1);
+
+    // 먼저, root 페이지가 하나만 남는 경우를 체크합니다.
+    if (!movedPage.parentId) {
+      const rootPages = pages.filter((page) => !page.parentId);
+      if (rootPages.length === 1) {
+        // 원상복귀
+        pages.splice(source.index, 0, movedPage);
+        setData({ ...data, pages });
+        return;
+      }
     }
-  
+
+    // 이동할 목적지 페이지 정보를 가져옵니다.
+    const targetPage = pages[destination.index];
+
+    if (targetPage) {
+      if (movedPage.parentId === targetPage.parentId) {
+        movedPage.parentId = targetPage.parentId;
+      } else {
+        // 상위 페이지로 이동
+        if (!targetPage.parentId) {
+          movedPage.parentId = undefined;
+        } else {
+          // 하위 페이지로 이동
+          movedPage.parentId = targetPage.parentId;
+        }
+      }
+    } else {
+      // 이 경우는 상위 페이지로 이동하는 것으로 판단하고 parentId를 제거합니다.
+      movedPage.parentId = undefined;
+    }
+
+    // 페이지 순서를 재배치합니다.
+    // 상위 페이지로 이동하는 경우는 페이지 목록 맨 끝으로 이동합니다.
+    if (movedPage.parentId === undefined) {
+      pages.push(movedPage);
+    } else {
+      pages.splice(destination.index, 0, movedPage);
+    }
+
     setData({ ...data, pages });
   };
 
@@ -119,22 +149,20 @@ function DndPage() {
                   <div
                     {...provided.draggableProps}
                     ref={provided.innerRef}
-                    className={index === 0 ? "font-bold" : "ml-4"}
+                    className={page.parentId ? "ml-4" : ""}
                   >
-                    <span {...provided.dragHandleProps}>
-                      {/* 토글 아이콘 */}
-                      <button
-                        onClick={() => {
-                          if (page.templates.length > 0) {
-                            togglePageExpand(page.id);
-                          }
-                        }}
-                      >
-                        {page.isExpanded ? "▼" : "▶"}
-                      </button>
-                      {/* 페이지 이름 */}
-                      {page.title}
-                    </span>
+                    <button
+                      {...provided.dragHandleProps}
+                      onClick={() => {
+                        if (page.templates.length > 0) {
+                          togglePageExpand(page.id);
+                        }
+                      }}
+                    >
+                      {page.isExpanded ? "▼" : "▶"}
+                    </button>
+                    {/* 페이지 이름 */}
+                    {page.title}
 
                     {/* 템플릿 목록 */}
                     {page.isExpanded &&
