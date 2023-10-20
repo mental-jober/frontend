0eaeeb7e5cc7b5ba20238d544c0169fd203efeae
ChaDongmin <cdm1263@gmail.com>
2023-10-20 21:43:15 +0900

    Refactor: 프로필 이미지의 여부에 따른 스페이스 타입 결정 로직 구현

--- src/app/space/[id]/edit/page.tsx
+++ src/app/space/[id]/edit/page.tsx
@@ -4,14 +4,14 @@ import DraggableBlocks from "@/components/spaceLayout/DraggableBlocks";
 import IntroProfile from "@/components/spaceLayout/IntroProfile";
 import IntroProject from "@/components/spaceLayout/IntroProject";
 import Plates from "@/components/spaceLayout/Plates";
-import { TempSpaceData, createBlock } from "@/lib/api/spaceEditAPI";
+import { createBlock } from "@/lib/api/spaceEditAPI";
 import { useToastStore } from "@/lib/store/store.module";
 import useComponentStore, {
   ComponentData,
 } from "@/lib/store/useComponentStore";
 import { useIsNewSpaceStore } from "@/lib/store/useIsNewSpace";
 import { usePageLayoutStore } from "@/lib/store/usePageLayoutStore";
-import useSpaceStore from "@/lib/store/useSpaceStore";
+import useSpaceStore, { SpaceData } from "@/lib/store/useSpaceStore";
 import useSpaceWallStore from "@/lib/store/useSpaceWallStore";
 import {
   useEnterEditQuery,
@@ -21,12 +21,14 @@ import {
 import ToastUi from "@/components/toast/ToastUi";
 
 import { useParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { modifySpaceData } from "@/lib/utils";
 
 const EditPage = () => {
   const { spaceWallId, setSpaceWallId } = useSpaceWallStore();
-  const { addData, getData } = useSpaceStore();
+  const [introType, setIntroType] = useState<string>("");
+  const { addData, getData, getValue } = useSpaceStore();
   const { getSpaceComponents, setSpaceComponents } = useComponentStore();
   const { isNewSpace, setIsNewSpace } = useIsNewSpaceStore();
   const { data: tempData } = useSpaceTempViewQuery(spaceWallId as number);
@@ -39,21 +41,52 @@ const EditPage = () => {
     },
   };
 
-  const modifiedData = {
-    ...getData(spaceWallId as number),
-    componentTempList: getData(spaceWallId as number)?.componentList,
+  useEffect(() => {
+    const getProfileImage = () => {
+      const imageURL = getValue(spaceWallId as number, "profileImageUrl");
+      if (imageURL !== null) {
+        if (imageURL === "") {
+          setIntroType("project");
+        } else {
+          setIntroType("profile");
+        }
+      }
+    };
+    getProfileImage();
+  }, [getValue, spaceWallId]);
+
+  console.log(getData(spaceWallId as number));
+  console.log(introType);
+
+  const renderIntro = (introType: string) => {
+    if (introType) {
+      if (introType === "project") {
+        return <IntroProject />;
+      } else {
+        return <IntroProfile />;
+      }
+    }
   };
 
-  delete modifiedData.componentList;
+  const modifiedData = modifySpaceData(
+    getData(spaceWallId as number) as SpaceData,
+  );
+
+  // const modifiedData = {
+  //   ...getData(spaceWallId as number),
+  //   componentTempList: getData(spaceWallId as number)?.componentList,
+  // };
+
+  // delete modifiedData.componentList;
 
   useSpaceTempSaveQuery(
     spaceWallId as number,
-    modifiedData as TempSpaceData,
+    modifiedData,
     spaceTempSaveQueryOption,
   );
 
   const paramSpaceWallId = useParams().id;
-  const { type, composition } = usePageLayoutStore();
+  const { composition } = usePageLayoutStore();
   const enterEditQueryOption = {
     refetchOnWindowFocus: false,
   };
@@ -70,10 +103,11 @@ const EditPage = () => {
     .sort((a, b) => a.sequence - b.sequence);
 
   useEffect(() => {
-    if (typeof paramSpaceWallId === "string") {
-      if (spaceWallId !== parseInt(paramSpaceWallId)) {
-        setSpaceWallId(parseInt(paramSpaceWallId));
-      }
+    if (
+      typeof paramSpaceWallId === "string" &&
+      spaceWallId !== parseInt(paramSpaceWallId)
+    ) {
+      setSpaceWallId(parseInt(paramSpaceWallId));
     }
   }, [spaceWallId, setSpaceWallId, paramSpaceWallId]);
 
@@ -108,7 +142,6 @@ const EditPage = () => {
     };
 
     if (tempData) {
-      console.log(tempData);
       addData(spaceWallId as number, tempData.data);
       tempData.data.componentTempList.forEach((component: ComponentData) => {
         setSpaceComponents(
@@ -136,7 +169,7 @@ const EditPage = () => {
   return (
     <>
       <Container>
-        {type === "프로젝트형" ? <IntroProject /> : <IntroProfile />}
+        {renderIntro(introType)}
         <Plates />
         <DraggableBlocks blockData={filteredBlockData} />
       </Container>
